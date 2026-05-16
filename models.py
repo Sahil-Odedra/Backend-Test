1bd2ec38458ced631511006b93b8618d2a7ace50
Sahil-Odedra <sahilodedra26@gmail.com>
2026-05-16 14:54:25 +0530

    functions defined

--- models.py
+++ models.py
@@ -1,32 +1,32 @@
-    import uuid
+import uuid
 
-    DB=[]
+DB=[]
 
-    def Add_To_List(data):
-        data['id']=str(uuid.uuid4())
-        DB.append(data)
-        return data
+def Add_To_List(data):
+    data['id']=str(uuid.uuid4())
+    DB.append(data)
+    return data
 
+def All_Movies():
+    return DB
 
-    def All_Movies():
-        return DB
+def search(id):
+    for i in DB:
+        if i['id']==id:
+            return i
+    return None
 
-    def search(id):
-        for i in DB:
-            if i['id']==id:
-                return i
-        return None
+def delete(id):
+    for i in DB:
+        if i['id'] ==id:
+            DB.remove(i)
+            return True
+    return False
 
-    def delete(id):
-        for i in DB:
-            if i['id'] ==id:
-                DB.remove(i)
-                return True
-        return False
-
-    def update(id,data):
-        for i in DB:
-            if i['id']==id:
-                DB['id']=data
-                return DB[id]
-        return None
+def update(id,data):
+    for i in range(len(DB)):
+        if DB[i]['id']==id:
+            data['id']=id
+            DB[i]=data
+            return DB[i]
+    return None
